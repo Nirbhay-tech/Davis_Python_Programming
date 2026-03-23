347fcca6940bc6c43292c4820257492241061a6e
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-03-23 19:56:32 +0530

    set operation

---
+++ Labwork/set_oper.py
@@ -0,0 +1,9 @@
+Set1 = {1, 2, 3, 4}
+Set2 = {3, 4, 5, 6}
+
+Intersection=Set1.intersection(Set2)  #intersection built in 
+Difference=Set1.difference(Set2)   #difference bulit in
+
+# to display
+print("Intersection =",Intersection)
+print("Difference =",Difference)
