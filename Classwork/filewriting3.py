7d8657210bd0c4fbd84bad0402ed7df0ea4e64c5
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-04-10 10:33:36 +0530

    Write in different line

---
+++ Classwork/filewriting3.py
@@ -0,0 +1,18 @@
+#program to write 20 sentences in different lines
+#opening file for written operation
+f=open("firstfile3.txt","w") 
+#blank list of sentences
+sentences=[]
+#writing 20 sentences into the file
+print("Write any 20 sentences : ")
+for x in range(20):
+    #input data from the user
+    sentence=input()
+    #inserting the sentences into the list
+    sentences.append(sentence)
+    print("--------------------------")
+#------------------------------------------------
+#writing data into the file
+f.writelines(sentences,"\n")
+#closing the file
+f.close()
