649940e717022f6099c8f66bcd8a10095dae4029
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-04-20 10:47:14 +0530

    array with element 1

---
+++ Classwork/NumPy/onesArray.py
@@ -0,0 +1,10 @@
+import numpy as np
+#ones array
+array5=np.ones((4,3))
+print(array5)
+print("-----------------------------------")
+#creating array (3,5,1) of int datatype
+array5=np.ones((3,5,1),dtype=int)
+print(array5)
+print("-----------------------------------")
+
