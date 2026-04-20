c908eb36ed41d024ef499581b20b223572dbc3b3
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-04-20 10:20:52 +0530

    zeros function in numpy

---
+++ Classwork/NumPy/zeros.py
@@ -0,0 +1,10 @@
+import numpy as np
+#array creation in numpy
+#default float datatype
+array3=np.zeros((3,5))
+print(array3)
+print("----------------------------")
+#change to int
+array3=np.zeros((3,5), dtype=int)
+print(array3)
+print("----------------------------")
