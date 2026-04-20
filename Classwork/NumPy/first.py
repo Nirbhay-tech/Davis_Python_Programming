4373c6ebba9c921d21a6ecdd2de082cc7008d6bc
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-04-20 10:05:18 +0530

    first numpy program

---
+++ Classwork/NumPy/first.py
@@ -0,0 +1,12 @@
+import numpy as np
+#creating array
+data=np.array([45,56,89,90,29])
+print(data)
+print("-------------------------------")
+#elements greater than 50
+print(data > 50)
+print("-------------------------------")
+#elements divided by 5
+numbers2=data/5
+print(numbers2)
+print("-------------------------------")
