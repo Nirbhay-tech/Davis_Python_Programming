004388862a2f4c21fa7d5d9b0543696fb33046f6
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-04-20 09:58:33 +0530

    numpy array creation

---
+++ Classwork/NumPy/CreateArray2.py
@@ -0,0 +1,4 @@
+import numpy as np
+#to create array
+numbers=np.array([20,30,40,7.7,90])
+print(numbers)
---
+++ Classwork/NumPy/CreateArray3.py
@@ -0,0 +1,4 @@
+import numpy as np
+#to create array
+numbers=np.array([20,30,40,"python",90])
+print(numbers)
